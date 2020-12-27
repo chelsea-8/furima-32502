a37c2cfc239c2819cae3075858931d5cb36337c5
chelsea-8 <chelsea.102321@gmail.com>
2020-12-27 21:33:17 +0900

    Revert "ユーザー管理機能"
    This reverts commit efdd615d42b69e43f88593bccb1dbcf6818ae446.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-validates :nike_name, presence: true
-validates :/\A[a-zA-Z0-9]+\z/ , presence: true
-
-has_many :items
-has_many :orders
-
 end
-
-
-# パスワードは、半角英数字混合での入力が必須であること
