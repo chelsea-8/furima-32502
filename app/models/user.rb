a82b015290e3002353188d500b8c654f6439ee7b
chelsea-8 <chelsea.102321@gmail.com>
2020-12-27 21:33:11 +0900

    Revert "ユーザー"
    This reverts commit 31f41537b3f1254d7decaa0b477d1158f6b03d00.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
 validates :nike_name, presence: true
-PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/ 
-validates_format_of :password, with: PASSWORD_REGEX 
-
+validates :/\A[a-zA-Z0-9]+\z/ , presence: true
 
 has_many :items
 has_many :orders
 
 end
+
 
 # パスワードは、半角英数字混合での入力が必須であること
