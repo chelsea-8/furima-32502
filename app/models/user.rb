31f41537b3f1254d7decaa0b477d1158f6b03d00
chelsea-8 <chelsea.102321@gmail.com>
2020-12-27 20:43:50 +0900

    ユーザー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
 validates :nike_name, presence: true
-validates :/\A[a-zA-Z0-9]+\z/ , presence: true
+PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/ 
+validates_format_of :password, with: PASSWORD_REGEX 
+
 
 has_many :items
 has_many :orders
 
 end
-
 
 # パスワードは、半角英数字混合での入力が必須であること
