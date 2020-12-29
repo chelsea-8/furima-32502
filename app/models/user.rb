130785ad0445a806b5a8df71509af2fb1e5391e9
chelsea-8 <chelsea.102321@gmail.com>
2020-12-29 19:54:29 +0900

    ユーザー

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nike_name, presence: true
+  with_options presence: true do
+    validates :nike_name
+    validates :barth_day
+  end
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は6文字以上で半角英数字をそれぞれ含めてください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,message: 'に全角文字を使用してください' } do
@@ -16,8 +20,6 @@ class User < ApplicationRecord
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
-
-  validates :barth_day, presence: true
 
 has_many :items
 has_many :orders
