e60d427e18f41e07d5fb9a6dd5e28d77f602dea9
chelsea-8 <chelsea.102321@gmail.com>
2021-01-08 18:58:42 +0900

    詳細

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :order 
 
   with_options presence: true do
     validates :product_name
@@ -21,4 +22,9 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_charge
+  belongs_to  :category
+  belongs_to  :condition
+  belongs_to  :prefecture
+  belongs_to  :days_to_ship
+
 end
