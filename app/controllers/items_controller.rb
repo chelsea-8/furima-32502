a4caf7cb327a6be409788a2313335a672f869293
chelsea-8 <chelsea.102321@gmail.com>
2021-01-09 17:13:32 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -11,10 +12,8 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
-
-
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -24,13 +23,28 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    redirect_to root_path if @item.order.present? || @item.user_id != current_user.id
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  
+
   private
   def item_params
     params.require(:item).permit(:product_name,:product_description,:category_id,:condition_id,:shipping_charge_id,
     :prefecture_id,:days_to_ship_id,:price,:image).merge(user_id: current_user.id)
-    
-
   end
 
-
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
+
